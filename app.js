fbbeed8f2796cf38288ebb649d1be4aa5d84f3ec
jaikhanna <jkhanna23@gmail.com>
2015-10-21 21:58:56 -0700

    modifications to styles

--- app.js
+++ app.js
@@ -1,19 +1,14 @@
 var app = require('express')();
 var http = require('http').Server(app);
-// var io = require('socket.io')(http);
-// var fs = require('fs'); 
 var request = require('request');
 var bodyParser = require('body-parser');
-app.use(bodyParser.json()); // support json encoded bodies
-app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function(req, res){
 
-	// res.sendfile('index.html');
 	res.sendFile(__dirname + '/index.html');
 });
-
-
 
 app.post('/newVideo', function(req, res){
 
@@ -21,22 +16,12 @@ app.post('/newVideo', function(req, res){
 
 	request({
 		uri: 'https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&q='+req.body.artistName+'&key='+apiKey,
-	}, function (error, response, body) {
-	  		if (!error && response.statusCode == 200) {
-	    		res.send(body); // Print the google web page.
-	    		
-	  		}
-	}
+		}, function (error, response, body) {
+		  		if (!error && response.statusCode == 200) {
+		    		res.send(body);  		
+		  		}
+		}
 	);
-
-	// request('https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&q='+req.body.artistName+'&key='+apiKey, function (error, response, body) {
-	//   		if (!error && response.statusCode == 200) {
-	//     		res.send(body); // Print the google web page.
-	    		
-	//   		}
-	// });
-
-
 });
 
 http.listen(3000, function(){
